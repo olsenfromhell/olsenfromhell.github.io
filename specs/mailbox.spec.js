8e93aee05b2b624507107643b2b123cf2d327ebf
makeevmr <makeevmr@m2.ru>
2022-06-04 16:24:26 +0300

    - test added - description added - html-report added to git-ignore

--- specs/mailbox.spec.js
+++ specs/mailbox.spec.js
@@ -1,20 +1,37 @@
 import api from '../framework/controllers';
 import BuildMail from '../builder/newMail';
 
-describe('Test', () => {
+describe('Тестирование сервиса mailbox', () => {
 
-  test('Доступ без API key', async () => {
+  const key = 'test123';
+
+  test('Доступ с некорректным API key', async () => {
+
     const user = new BuildMail().addUsername().addDomainName()
         .generate();
     const mail = `${user.username}@${user.domainname}`;
 
-    const response = await api().MailboxUser().get('', mail);
+    const response = await api().MailboxUser().get(key, mail);
     const data = await response.json();
     expect(response.status).toEqual(200);
     expect(data.success).toEqual(false);
     expect(data.error.code).toEqual(101);
-    expect(data.error.type).toEqual('missing_access_key');
-    expect(data.error.info).toEqual('You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]');
-  });
-});
+    expect(data.error.type).toEqual('invalid_access_key');
+    expect(data.error.info).toEqual('You have not supplied a valid API Access Key. [Technical Support: support@apilayer.com]');
 
+  });
+
+    test('Доступ без API key', async () => {
+      const user = new BuildMail().addUsername().addDomainName()
+          .generate();
+      const mail = `${user.username}@${user.domainname}`;
+
+      const response = await api().MailboxUser().get('', mail);
+      const data = await response.json();
+      expect(response.status).toEqual(200);
+      expect(data.success).toEqual(false);
+      expect(data.error.code).toEqual(101);
+      expect(data.error.type).toEqual('missing_access_key');
+      expect(data.error.info).toEqual('You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]');
+    });
+  });
